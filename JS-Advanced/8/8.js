a8d36d3198cfd423de5d0741ded6e2aeac4fb48b
NikolaKasabov <n_kasabov@mail.bg>
2019-02-25 09:47:32 +0200

    add file

--- JS-Advanced/8/8.js
+++ JS-Advanced/8/8.js
@@ -12,8 +12,8 @@ class Kitchen {
       quantity = +quantity;
       price = +price;
 
-      if (this.budget >= quantity * price) {
-        this.budget -= (quantity * price);
+      if (this.budget >= price) {
+        this.budget -= price;
         if (!this.productsInStock.hasOwnProperty(productName)) {
           this.productsInStock[productName] = quantity;
         } else {
@@ -42,7 +42,28 @@ class Kitchen {
 
     return `The ${meal} is already in our menu, try something different.`;
   }
+
+  showTheMenu() {
+    let result = [];
+    if (Object.keys(this.menu).length === 0) {
+      return 'Our menu is not ready yet, please come later...';
+    }
+
+    Object.entries(this.menu).forEach((arr) => {
+      result.push(`${arr[0]} - $ ${arr[1].price}`);
+    });
+
+    return result.join('\n');
+  }
 }
+
+
 
 let kitchen = new Kitchen(1000);
 console.log(kitchen.loadProducts(['Banana 10 5', 'Banana 20 10', 'Strawberries 50 30', 'Yogurt 10 10', 'Yogurt 500 1500', 'Honey 5 50']));
+
+console.log(kitchen.addToMenu('frozenYogurt', ['Yogurt 1', 'Honey 1', 'Banana 1', 'Strawberries 10'], 9.99));
+console.log(kitchen.addToMenu('Pizza', ['Flour 0.5', 'Oil 0.2', 'Yeast 0.5', 'Salt 0.1', 'Sugar 0.1', 'Tomato sauce 0.5', 'Pepperoni 1', 'Cheese 1.5'], 15.55));
+
+console.log(kitchen.showTheMenu());
+// kitchen.showTheMenu();
