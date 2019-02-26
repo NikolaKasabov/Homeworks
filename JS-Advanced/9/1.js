3db4b99b47408cbb9cb7d83a21bac8007f6b48ee
NikolaKasabov <n_kasabov@mail.bg>
2019-02-26 16:21:08 +0200

    add file

--- JS-Advanced/9/1.js
+++ JS-Advanced/9/1.js
@@ -1,14 +1,37 @@
-function solve() {
-  class Rectangle {
+function solve(inputArr) {
+  const result = [];
 
+  class Rectangle {
+    constructor(width, height) {
+      this.width = width;
+      this.height = height;
+    }
+
+    area() {
+      return this.width * this.height;
+    }
+
+    compareTo(other) {
+      if (this.area() === other.area()) {
+        return other.width - this.width;
+      } else {
+        return other.area() - this.area();
+      }
+    }
   }
+
+  for (const arr of inputArr) {
+    let currentRect = new Rectangle(arr[0], arr[1]);
+    result.push(currentRect);
+  }
+
+  result.sort((a, b) => a.compareTo(b));
+
+  return result;
 }
 
-
-solve(
+console.log(solve(
   [
-    [10, 5],
-    [3, 20],
-    [5, 12]
+    [1, 20], [20, 1], [5, 3], [5, 3]
   ]
-)
+));
