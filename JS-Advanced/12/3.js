df36876f022b3e31034027c25b539cf15dce0f91
NikolaKasabov <n_kasabov@mail.bg>
2019-03-03 15:58:51 +0200

    add file

---
+++ JS-Advanced/12/3.js
@@ -0,0 +1,80 @@
+function validate() {
+  let nameElem = $('input#username');
+  let emailElem = $('input#email');
+  let passwordElem = $('input#password');
+  let repeatPasswordElem = $('input#confirm-password');
+  let companyNumberElem = $('input#companyNumber');
+  let shouldValidateCompanyNumber = false;
+
+  const nameRegex = /^[a-zA-Z0-9]{3,20}$/;
+  const passwordRegex = /^.{5,15}$/;
+  const emailRegex = /^[^@]*@[^.]*\..*$/;
+
+  function showOrHideCompanyFieldset() {
+    if ($('#company')[0].checked) {
+      $('fieldset#companyInfo').show();
+      shouldValidateCompanyNumber = true;
+    } else {
+      $('fieldset#companyInfo').hide();
+      shouldValidateCompanyNumber = false;
+    }
+  }
+
+  $('input#company').change(showOrHideCompanyFieldset);
+
+  function validate(e) {
+    e.preventDefault();
+
+    let isAllValid = true;
+    let isNameValid = nameRegex.test(nameElem.val());
+    let isEmailValid = emailRegex.test(emailElem.val());
+    let isPasswordValid = passwordRegex.test(passwordElem.val());
+    let arePasswordsSame = passwordElem.val() === repeatPasswordElem.val();
+    let isCompanyNumberValid = (+companyNumberElem.val() >= 1000 && +companyNumberElem.val() <= 9999);
+
+    if (isNameValid) {
+      nameElem.css('border-color', '');
+    } else {
+      nameElem.css('border-color', 'red');
+      isAllValid = false;
+    }
+
+    if (isEmailValid) {
+      emailElem.css('border-color', '');
+    } else {
+      emailElem.css('border-color', 'red');
+      isAllValid = false;
+    }
+
+    if (isPasswordValid) {
+      passwordElem.css('border-color', '');
+    } else {
+      passwordElem.css('border-color', 'red');
+      isAllValid = false;
+    }
+
+    if (arePasswordsSame && isPasswordValid) {
+      repeatPasswordElem.css('border-color', '');
+    } else {
+      repeatPasswordElem.css('border-color', 'red');
+      isAllValid = false;
+    }
+
+    if (shouldValidateCompanyNumber) {
+      if (isCompanyNumberValid) {
+        companyNumberElem.css('border-color', '');
+      } else {
+        companyNumberElem.css('border-color', 'red');
+        isAllValid = false;
+      }
+    }
+
+    if (isAllValid) {
+      $('div#valid').show();
+    } else {
+      $('div#valid').hide();      
+    }
+  }
+
+  $('button#submit').click(validate);
+}
