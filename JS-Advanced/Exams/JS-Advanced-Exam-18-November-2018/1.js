686e79110b011a8bf9af7ae62f7930d4ea8030c6
n <n_kasabov@mail.bg>
2019-03-06 18:31:51 +0200

    add file

--- JS-Advanced/Exams/JS-Advanced-Exam-18-November-2018/1.js
+++ JS-Advanced/Exams/JS-Advanced-Exam-18-November-2018/1.js
@@ -1,12 +1,28 @@
 function acceptance() {
-  let company = $('input[name="shippingCompany"]').val();
-  let product = $('input[name="productName"]').val();
-  let quantity = $('input[name="productQuantity"]').val();
-  let scrape = $('input[name="productScrape"]').val();
+  let companyElem = $('input[name="shippingCompany"]');
+  let productElem = $('input[name="productName"]');
+  let quantityElem = $('input[name="productQuantity"]');
+  let scrapeElem = $('input[name="productScrape"]');
 
-  if (!company || !product || !+quantity || !+scrape) {
+  let company = companyElem.val();
+  let product = productElem.val();
+  let quantity = +quantityElem.val();
+  let scrape = +scrapeElem.val();
+
+  companyElem.val('');
+  productElem.val('');
+  quantityElem.val('');
+  scrapeElem.val('');
+
+  if (!company || !product || !quantity || !scrape || (quantity - scrape <= 0)) {
     return;
   }
 
+  let div = $('<div>');
+  let p = $('<p>');
+  p.text(`[${company}] ${product} - ${quantity - scrape} pieces`);
+  let button = $('<button>Out of stock</button>').click(() => div.remove());
+  div.append(p, button);
 
+  $('div#warehouse').append(div);
 }
