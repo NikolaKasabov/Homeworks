e99520e072ea1076aa7f7118e763aeec8d38e77e
n <n_kasabov@mail.bg>
2019-03-07 07:00:26 +0200

    add file

--- testing.js
+++ testing.js
@@ -1,27 +1,7 @@
-function validate() {
-  let nameElem = $('input#username');
-  let emailElem = $('input#email');
-  let passwordElem = $('input#password');
-  let repeatPasswordElem = $('input#confirm-password');
-  let companyNumberElem = $('input#companyNumber');
-
-  const nameRegex = /^[a-zA-Z0-9]{3,20}$/;
-  const passwordRegex = /^.{5,15}$/;
-  const emailRegex = /^[^@]*@[^.]*\..*$/;
-
-  function showOrHideCompanyFieldset() {
-    if ($('#company')[0].checked) {
-      $('fieldset#companyInfo').show();
-    } else {
-      $('fieldset#companyInfo').hide();
-    }
-  }
-
-  $('input#company').change(showOrHideCompanyFieldset);
-
-  function validate() {
-    
-  }
-
-  $('button#submit').click(validate);
+let kids = {
+  6: [],
+  5: [],
+  4: [],
+  3: [],
+  2: []
 }
