c2f0e753d0ce26ae7d87e1114e020b3d28315577
n <n_kasabov@mail.bg>
2019-03-12 07:11:53 +0200

    add file

--- test/2.Calculator.js
+++ test/2.Calculator.js
@@ -59,7 +59,95 @@ describe('Calculator testing', function () {
   let calc;
   beforeEach(function () { calc = new Calculator() });
 
-  it('first', function () {
-    assert.isObject(calc.expenses);
+  describe('property expenses', function () {
+    it('is array', function () {
+      assert.isArray(calc.expenses);
+      assert.deepEqual(calc.expenses, []);
+    });
+  });
+
+  describe('Function add(data)', function () {
+    it('empty input', function () {
+      calc.add();
+      assert.deepEqual(calc.expenses, [undefined]);
+    });
+    it('correct input', function () {
+      calc.add(1);
+      assert.equal(calc.expenses[0], 1);
+    });
+    it('invalid input', function () {
+      calc.add([]);
+      assert.isArray(calc.expenses[0]);
+    });
+    it('invalid input', function () {
+      calc.add({});
+      assert.isObject(calc.expenses[0]);
+    });
+  });
+
+  describe('Function divideNums() ', function () {
+    it('valid input', function () {
+      calc.add(1);
+      calc.add(2);
+      calc.add(3);
+      calc.divideNums();
+      assert.closeTo(calc.expenses[0], 0.166, 0.001);
+    });
+
+    it('mixed input', function () {
+      calc.add(1);
+      calc.add(2);
+      calc.add('some text');
+      calc.add(3);
+      calc.divideNums();
+      assert.closeTo(calc.expenses[0], 0.166, 0.001);
+    });
+
+    it('divide by zero', function () {
+      calc.add(1);
+      calc.add(0);
+      calc.add(3);
+      assert.equal(calc.divideNums(), 'Cannot divide by zero');
+    });
+
+    it('no numbers', function () {
+      calc.add('asd');
+      calc.add(['t']);
+      calc.add({ one: 1 });
+      assert.throws(() => calc.divideNums(), 'There are no numbers in the array!');
+    });
+  });
+
+  describe('Function toString() ', function () {
+    it('no input', function () {
+      assert.equal(calc.toString(), 'empty array');
+    });
+
+    it('valid input', function () {
+      calc.add(1);
+      calc.add(2);
+      calc.add(3);
+      assert.equal(calc.toString(), '1 -> 2 -> 3');
+    });
+  });
+
+  describe('Function orderBy() ', function () {
+    it('no input', function () {
+      assert.equal(calc.orderBy(), 'empty');
+    });
+
+    it('numbers input', function () {
+      calc.add(3);
+      calc.add(1);
+      calc.add(2);
+      assert.equal(calc.orderBy(), '1, 2, 3');
+    });
+
+    it('mixed input', function () {
+      calc.add(3);
+      calc.add('some text');
+      calc.add(2);
+      assert.equal(calc.orderBy(), '2, 3, some text');
+    });
   });
 });
