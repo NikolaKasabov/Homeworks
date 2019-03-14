7e9c77181b62f47d4e249651d4bfd87e1deaee10
n <n_kasabov@mail.bg>
2019-03-14 06:44:56 +0200

    add file

---
+++ JS-Advanced/Exams/JS Advanced Retake Exam - 30 August 2018/3.Vacationer.js
@@ -0,0 +1,15 @@
+class Vacationer {
+  constructor(namesArr) {
+    this.fullName = { firstName: namesArr[0], middleName: namesArr[1], lastName: namesArr[2] };
+    this.idNumber;
+
+  }
+
+  get fullName() {
+    return this._fullname;
+  }
+
+  set fullName(namesArr) {
+    if ()
+  }
+}
