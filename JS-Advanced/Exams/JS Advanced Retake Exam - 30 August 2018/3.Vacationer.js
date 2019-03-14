d892149850319b19d534b6b602e678f37268e62f
NikolaKasabov <n_kasabov@mail.bg>
2019-03-14 16:09:16 +0200

    add

--- JS-Advanced/Exams/JS Advanced Retake Exam - 30 August 2018/3.Vacationer.js
+++ JS-Advanced/Exams/JS Advanced Retake Exam - 30 August 2018/3.Vacationer.js
@@ -1,11 +1,16 @@
 class Vacationer {
   constructor(namesArr, creditCardArr) {
     this.fullName = namesArr;
+
+    if (creditCardArr && (typeof creditCardArr[0] !== 'number' || typeof creditCardArr[1] !== 'string' || typeof creditCardArr[2] !== 'number')) {
+      throw new Error('Invalid credit card details');
+    }
     this.creditCard = {
       cardNumber: creditCardArr ? creditCardArr[0] : 1111,
       expirationDate: creditCardArr ? creditCardArr[1] : '',
       securityNumber: creditCardArr ? creditCardArr[2] : 111
     };
+
     this.wishList = [];
     this.idNumber = this.generateIDNumber(namesArr);
   }
@@ -37,9 +42,9 @@ class Vacationer {
     let vowels = ["a", "e", "o", "i", "u"];
     let lastNameLastLetter = namesArr[2][namesArr[2].length - 1];
     if (vowels.includes(lastNameLastLetter)) {
-      id += 8;
+      id += '8';
     } else {
-      id += 7;
+      id += '7';
     }
 
     return id;
@@ -49,10 +54,33 @@ class Vacationer {
     if (inputArr.length < 3) {
       throw new Error('Missing credit card information');
     }
+    // ??? strings or numbers - inputArr[0] and [2]
+    if (typeof (inputArr[0]) !== 'number' || typeof (inputArr[1]) !== 'string' || typeof (inputArr[2]) !== 'number') {
+      throw new Error('Invalid credit card details');
+    }
+
+    this.creditCard.cardNumber = inputArr[0];
+    this.creditCard.expirationDate = inputArr[1];
+    this.creditCard.securityNumber = inputArr[2];
+  }
+
+  addDestinationToWishList(destination) {
+    if (this.wishList.includes(destination)) {
+      throw new Error('Destination already exists in wishlist');
+    }
+
+    this.wishList.push(destination);
+    this.wishList.sort((a, b) => a.length - b.length);
+  }
+
+  getVacationerInfo() {
+    let wishListString = this.wishList.join(', ') || 'empty';
+    let result = `Name: ${this.fullName.firstName} ${this.fullName.middleName} ${this.fullName.lastName}\n`;
+    result += `ID Number: ${this.idNumber}\nWishlist:\n${wishListString}\nCredit Card:\n`;
+    result += `Card Number: ${this.creditCard.cardNumber}\nExpiration Date: ${this.creditCard.expirationDate}\nSecurity Number: ${this.creditCard.securityNumber}\n`;
+    return result;
   }
 }
-
-
 
 // Initialize vacationers with 2 and 3 parameters
 let vacationer1 = new Vacationer(["Vania", "Ivanova", "Zhivkova"]);
@@ -64,8 +92,6 @@ try {
 } catch (err) {
   console.log("Error: " + err.message);
 }
-
-console.log(vacationer1.testing);
 
 // Should throw an error (Missing credit card information)
 try {
--- testing.js
+++ testing.js
@@ -1,21 +1,4 @@
-class Person {
-  constructor(value) {
-    
-  }
 
-  get name() {
-    return this._name;
-  }
+let a = [].join(', ');
 
-  set name(value) {
-    console.log(value);
-    this._name = { ime: value };
-    // this._name = name;
-  }
-}
-
-let p = new Person('Pesho');
-// p.name = 'Gosho';
-
-// console.log(0 || '' || false);
-// console.log(5 && true && 'false' && 'asd');
+console.log(typeof a === 'number');
