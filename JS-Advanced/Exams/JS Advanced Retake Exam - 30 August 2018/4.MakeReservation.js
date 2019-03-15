cd03eef379539b818d91eb5a67bbd21669ee5196
NikolaKasabov <n_kasabov@mail.bg>
2019-03-15 10:03:49 +0200

    change

--- JS-Advanced/Exams/JS Advanced Retake Exam - 30 August 2018/4.MakeReservation.js
+++ JS-Advanced/Exams/JS Advanced Retake Exam - 30 August 2018/4.MakeReservation.js
@@ -2,6 +2,39 @@ function makeReservation(selector) {
   let inputObj = {
     'Name': '',
     'E-Mail': '',
-    
+    'Phone': 0,
+    'Address': '',
+    'Postal Code': 0
+  };
+
+  let nameEl = $('#fullName');
+  let emailEl = $('#email');
+  let phoneNumberEl = $('#phoneNumber');
+  let addressEl = $('#address');
+  let postalCodeEl = $('#postalCode');
+  let submitBtn = $('#submit');
+  let ulEl = $('#infoPreview');
+  let editBtn = $('#edit');
+  let continueBtn = $('#continue');
+  let containerEl = $('#container');
+
+  function enableSubmitBtn() {
+    submitBtn.attr('disabled', false);
   }
+
+  function disableSubmitBtn() {
+    submitBtn.attr('disabled', true);
+  }
+
+  function enableEditAndContinueBtn() {
+    editBtn.attr('disabled', false);
+    continueBtn.attr('disabled', false);
+  }
+
+  function disableEditAndContinueBtn() {
+    editBtn.attr('disabled', true);
+    continueBtn.attr('disabled', true);
+  }
+
+  
 }
