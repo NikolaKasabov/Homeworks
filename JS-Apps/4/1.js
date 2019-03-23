5e76cfbc6346fcf7564f5f318c2d2e32a9cf7a9b
n <n_kasabov@mail.bg>
2019-03-23 12:29:38 +0200

    a

--- JS-Apps/4/1.js
+++ JS-Apps/4/1.js
@@ -1,3 +1,12 @@
 function loadRepos() {
-  
+  let xhr = new XMLHttpRequest();
+  let divEl = document.querySelector('div#res');
+
+  xhr.open('get', 'https://api.github.com/users/testnakov/repos');
+  xhr.onreadystatechange = () => {
+    if (xhr.readyState === 4) {
+      divEl.textContent = xhr.responseText;
+    }
+  };
+  xhr.send();
 }
