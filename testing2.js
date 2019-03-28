7066fb537ee580f9cffb36526968aa842b5b03fb
NikolaKasabov <n_kasabov@mail.bg>
2019-03-28 16:56:56 +0200

    ch

---
+++ testing2.js
@@ -0,0 +1,24 @@
+async function f1() {
+  let p = new Promise(resolve => {
+    setTimeout(() => {
+      resolve('f1');
+    }, 2000);
+  });
+
+  let result;
+  result = await p;
+  console.log(result);
+}
+
+function f2() {
+  console.log('sync func f2');
+}
+
+
+f1();
+
+setTimeout(() => {
+  console.log('set timeout');
+}, 2000);
+
+f2();
