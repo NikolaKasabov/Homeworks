a4c0e314d8b36131a1b3a920ecb9c74aab11cc03
NikolaKasabov <n_kasabov@mail.bg>
2019-04-01 16:54:27 +0300

    add

---
+++ JS-Apps/7/7.SecretKnock.js
@@ -0,0 +1,47 @@
+function attachEvents() {
+  const baseUrl = 'https://baas.kinvey.com/appdata/kid_BJXTsSi-e/knock';
+  const authString = 'Basic ' + btoa('guest:guest');
+  const $startBtn = $('button.start');
+  const $result = $('div.result');
+
+  let message = 'Knock Knock.';
+  let answer = '';
+
+  function checkForResponsePromise() {
+    return $.ajax({
+      method: 'GET',
+      url: baseUrl + `?query=${message}`,
+      headers: {
+        Authorization: authString
+      },
+      success: onSuccess,
+      error: err => console.log(err)
+    });
+  }
+
+  // displays data
+  function onSuccess(data) {
+    message = data.message;
+    answer = data.answer;
+
+    console.log(message);
+
+    if (message !== 'undefined') {
+      $result.append(`<p>${answer}</p>`);      
+    }
+
+    if (answer !== 'undefined') {
+      $result.append(`<p>${message}</p>`);      
+    }
+  }
+
+  async function start() {
+    $result.append(`<p>${message}</p>`);
+    
+    while (message !== undefined) {
+      await checkForResponsePromise();
+    }
+  }
+
+  $startBtn.on('click', start);
+}
