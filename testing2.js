31a331db809e1aa52ec56b137b0078dc2c6b6e1f
n <n_kasabov@mail.bg>
2019-04-02 08:05:23 +0300

    add

--- testing2.js
+++ testing2.js
@@ -1,8 +1,18 @@
-// while (true){
-//   console.log(1);
-//   break;
-// }
-do {
+function printAfter2Sec() {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      console.log('after 2 seconds');
+      resolve();
+    }, 2000);
+  });
+}
+
+async function solve() {
   console.log(1);
-  break;
-} while (true);
+
+  await printAfter2Sec();
+
+  console.log(2);
+}
+
+solve();
