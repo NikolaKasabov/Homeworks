ea84cd0e913e23c36e45b1302fd4324ee6626e96
NikolaKasabov <n_kasabov@mail.bg>
2019-04-02 16:51:27 +0300

    ch

--- JS-Apps/7/8.WildWildWest/attachEvents.js
+++ JS-Apps/7/8.WildWildWest/attachEvents.js
@@ -2,11 +2,134 @@ function attachEvents() {
   const baseUrl = 'https://baas.kinvey.com/appdata';
   const appKey = 'kid_HyJ875xtV';
   const collection = 'players';
-  const authString = 'Basic ' + btoa('guest:guest');
+  const authString = `Basic ${btoa('guest:guest')}`;
+  const $players = $('div#players');
+  const $newPlayerName = $('input#addName');
+  const $addPlayerBtn = $('button#addPlayer');
+  let players;
 
-  async function test() {
-    let result = await $.ajax({
-      
+  // get players from kinvey
+  async function getPlayers() {
+    console.log('in get players...');
+    try {
+      players = await $.ajax({
+        method: 'GET',
+        url: `${baseUrl}/${appKey}/${collection}`,
+        headers: {
+          Authorization: authString,
+        },
+      });
+    } catch (error) {
+      console.log(error);
+    }
+
+    displayPlayers(players);
+  }
+
+  getPlayers();
+
+  // display all players in div#players
+  function displayPlayers(playersArr) {
+    $players.empty();
+
+    playersArr.forEach((player) => {
+      const playerTemplate = `<div class="player" data-id="${player._id}">
+        <div class="row">
+          <label>Name:</label>
+          <label class="name">${player.name}</label>
+        </div>
+        <div class="row">
+          <label>Money:</label>
+          <label class="money">${player.money}</label>
+        </div>
+        <div class="row">
+          <label>Bullets:</label>
+          <label class="bullets">${player.bullets}</label>
+        </div>
+        <button class="play">Play</button>
+        <button class="delete">Delete</button>
+      </div>`;
+
+      $players.append(playerTemplate);
     });
+
+    // add click event handlers to 'Play' and 'Delete' buttons
+    $('button.play').on('click', onPlayClick);
+    $('button.delete').on('click', onDeleteClick);
+  }
+
+  // add new player to kinvey
+  function onAddPlayerClick() {
+    const name = $newPlayerName.val();
+    $.ajax({
+      method: 'POST',
+      url: `${baseUrl}/${appKey}/${collection}`,
+      headers: {
+        Authorization: authString,
+      },
+      data: {
+        name,
+        money: 500,
+        bullets: 6,
+      },
+      success: () => getPlayers(),
+      error: err => console.log(err),
+    });
+  }
+
+  // add click event listener to 'Add Player' button
+  $addPlayerBtn.on('click', onAddPlayerClick);
+
+  // on click 'Play' button
+  async function onPlayClick(ev) {
+    const currentId = $(ev.target).parent().data('id');
+
+    // TODO...
+    try {
+      const currentPlayer = await $.ajax({
+        method: 'GET',
+        url: `${baseUrl}/${appKey}/${collection}/${currentId}`,
+        headers: {
+          Authorization: authString,
+        },
+      });
+      saving(currentId);   ///////
+      showCanvasSaveReload();
+      loadCanvas({
+        name: currentPlayer.name,
+        money: Number(currentPlayer.money),
+        bullets: Number(currentPlayer.bullets),
+      });
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  // on click 'Delete' button
+  function onDeleteClick(ev) {
+    const currentId = $(ev.target).parent().data('id');
+
+    // TODO...
+  }
+
+  // on click 'Save' button
+  function onSaveClick(ev) {
+    // TODO...
+  }
+
+  function saving(playerId) {
+    
+  }
+
+  function showCanvasSaveReload() {
+    $('button#save').show();
+    $('button#reload').show();
+    $('canvas').show();
+  }
+
+  function hideCanvasSaveReload() {
+    $('button#save').hide();
+    $('button#reload').hide();
+    $('canvas').hide();
   }
 }
--- JS-Apps/7/8.WildWildWest/loadCanvas.js
+++ JS-Apps/7/8.WildWildWest/loadCanvas.js
@@ -1,30 +1,30 @@
 function loadCanvas(player) {
-  let canvas = document.getElementById("canvas");
-  let ctx = canvas.getContext("2d");
+  const canvas = document.getElementById('canvas');
+  const ctx = canvas.getContext('2d');
 
-  let background = new Image();
-  let target = new Image();
-  let sight = new Image();
+  const background = new Image();
+  const target = new Image();
+  const sight = new Image();
 
-  ctx.font = "24px serif";
-  ctx.fillStyle = "yellow";
+  ctx.font = '24px serif';
+  ctx.fillStyle = 'yellow';
 
-  let targetObj = { speed: 50, x: 0, y: 100, width: 300, height: 500 };
-  let mouse = { x: 0, y: 100 };
+  const targetObj = {speed: 50, x: 0, y: 100, width: 300, height: 500};
+  const mouse = { x: 0, y: 100 };
 
-  background.src = "imgs/west.jpg";
-  target.src = "imgs/target.png";
-  sight.src = "imgs/sight.png";
+  background.src = 'imgs/west.jpg';
+  target.src = 'imgs/target.png';
+  sight.src = 'imgs/sight.png';
 
-  let backgroundPromise = new Promise((resolve, reject) => {
+  const backgroundPromise = new Promise((resolve, reject) => {
     $(background).on('load', resolve);
   });
 
-  let targetPromise = new Promise((resolve, reject) => {
+  const targetPromise = new Promise((resolve, reject) => {
     $(target).on('load', resolve);
   });
 
-  let sightPromise = new Promise((resolve, reject) => {
+  const sightPromise = new Promise((resolve, reject) => {
     $(sight).on('load', resolve);
   });
 
@@ -40,23 +40,23 @@ function loadCanvas(player) {
     ctx.fillText(`Bullets: ${player.bullets}`, 650, 75);
 
     if (player.bullets === 0) {
-      ctx.font = "48px serif";
-      ctx.fillStyle = "red";
-      ctx.fillText(`Reload!`, canvas.width / 2 - 50, canvas.height / 2);
-      ctx.font = "24px serif";
-      ctx.fillStyle = "yellow";
+      ctx.font = '48px serif';
+      ctx.fillStyle = 'red';
+      ctx.fillText('Reload!', canvas.width / 2 - 50, canvas.height / 2);
+      ctx.font = '24px serif';
+      ctx.fillStyle = 'yellow';
     }
 
     ctx.drawImage(sight, mouseX, mouseY, 50, 50);
   }
 
-  $(canvas).mousemove(function (event) {
+  $(canvas).mousemove((event) => {
     mouse.x = event.clientX - 35;
     mouse.y = event.clientY - 20;
     drawCanvas(mouse.x, mouse.y);
   });
 
-  $(canvas).click(function (event) {
+  $(canvas).click((event) => {
     if (player.bullets > 0) {
       if (event.clientX > targetObj.x && event.clientX < targetObj.x + targetObj.width
         && event.clientY > targetObj.y && event.clientY < targetObj.y + targetObj.height) {
@@ -67,18 +67,17 @@ function loadCanvas(player) {
     }
   });
 
-  //Set a property in the Canvas to allow for cleaning the interval from outside
+  // Set a property in the Canvas to allow for cleaning the interval from outside
   canvas.intervalId = setInterval(moveTarget, 100);
 
   function moveTarget() {
     if (targetObj.speed > 0 && targetObj.x + targetObj.width >= canvas.width) {
       targetObj.speed = 0 - targetObj.speed;
-    }
-    else if (targetObj.speed < 0 && targetObj.x <= 0) {
+    } else if (targetObj.speed < 0 && targetObj.x <= 0) {
       targetObj.speed = 0 - targetObj.speed;
     }
 
     targetObj.x += targetObj.speed;
-    drawCanvas(mouse.x, mouse.y)
+    drawCanvas(mouse.x, mouse.y);
   }
 }
