accaf5d30b76c2f8fbd93532bd846394c3b70513
NikolaKasabov <n_kasabov@mail.bg>
2019-04-02 10:57:48 +0300

    ch

--- JS-Apps/7/8.WildWildWest/attachEvents.js
+++ JS-Apps/7/8.WildWildWest/attachEvents.js
@@ -1,3 +1,12 @@
 function attachEvents() {
-  //TODO
+  const baseUrl = 'https://baas.kinvey.com/appdata';
+  const appKey = 'kid_HyJ875xtV';
+  const collection = 'players';
+  const authString = 'Basic ' + btoa('guest:guest');
+
+  async function test() {
+    let result = await $.ajax({
+      
+    });
+  }
 }
