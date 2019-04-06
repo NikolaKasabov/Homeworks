05ef6ae63a0b354d973eca685eaa7a5dc7af9e03
n <n_kasabov@mail.bg>
2019-04-06 22:46:32 +0300

    c

--- testing2.js
+++ testing2.js
@@ -1,20 +1,34 @@
-const app = Sammy('#container', function () {
-  this.get('#/home', function () {
-    this.swap('<h2>Home Page</h2>');
+$(() => {
+  const app = Sammy('#container', function () {
+    this.route('get', '#/', function (context) {
+      this.swap('<h2>Home Page</h2>');
+    });
+
+    this.get('#/about', function (context) {
+      this.swap('<h2>About Page</h2>');
+    });
+
+    this.get('#/about/:productId', function (context) {
+      console.log(context.params);
+    });
+
+    this.get('#/login', function (context) {
+      this.swap(`<h2>Login Page</h2>
+      <form action="#/login" method="post">
+      User: <input name="user" type="text">
+      Pass: <input name="pass" type="password">
+      <input type="submit" value="Login">
+    </form>`);
+    });
+
+    this.post('#/login', function (context) {
+      console.log(context.params);
+    });
+
+    this.get('.', function () {
+      this.swap('********');
+    });
   });
 
-  this.get('#/about', function () {
-    this.swap('<h2>About Page</h2>');
-  });
-
-  this.get('#/login', function () {
-    this.swap('<h2>Login Page</h2>');
-  });
-
-  this.get('.', function () {
-    this.swap('********');
-  });
+  app.run();
 });
-
-
-$(() => app.run());
