cb3466d7b3a0751b35c27d3c56b4f307b62f8118
NikolaKasabov <n_kasabov@mail.bg>
2019-10-03 08:41:34 +0300

    deleted workshop

--- 6.JS-Back-end/6.Cubicle-Workshop-Part-1/index.js
+++
@@ -1,9 +0,0 @@
-const env = process.env.NODE_ENV || 'development';
-
-const app = require('express')();
-const config = require('./config/config')[env];
-
-require('./config/express')(app);
-require('./config/routes')(app);
-
-app.listen(config.port, console.log(`Listening on port ${config.port}! Now its up to you...`));
