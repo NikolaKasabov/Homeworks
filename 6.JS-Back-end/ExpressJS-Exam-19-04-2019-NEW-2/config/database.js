81beda4073ea1ec50c20291f8bdca483136867cd
NikolaKasabov <n_kasabov@mail.bg>
2019-11-03 10:11:19 +0200

    added exam task

---
+++ 6.JS-Back-end/ExpressJS-Exam-19-04-2019-NEW-2/config/database.js
@@ -0,0 +1,15 @@
+const mongoose = require('mongoose');
+
+// local mongodb
+const connectionUrl = 'mongodb://127.0.0.1:27017/exam-26102019-nkasabov';
+
+// mongodb Atlas cluster
+// const connectionUrl = 'mongodb+srv://user1:pass1@cluster0-ej5ez.mongodb.net/test?retryWrites=true&w=majority';
+
+mongoose.pluralize(null);
+
+module.exports = mongoose.connect(connectionUrl,
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  });
